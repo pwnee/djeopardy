4ddc3ce40c01644e4bb6258c40061a3d39e1d7ee
pwnee <wtbull@gmail.com>
2013-03-15 10:39:36 -0700

    Update settings.py

--- djeopardy/djeopardy/settings.py
+++ djeopardy/djeopardy/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'djeopardy',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'admin',
-        'PASSWORD': 'theR3dbu11',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
